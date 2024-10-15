6087d115c7cab543c26031aa799adb4c15dc7a44
Sakaef03 <103153340+Sakaef03@users.noreply.github.com>
2024-10-15 09:56:15 -0300

    Update LoginTester.java

--- LoginTester.java
+++ LoginTester.java
@@ -1,7 +1,6 @@
 public class LoginTester {
     public static void main(String[] args) throws Exception {
         LoginFactory factory = new LoginFactory();
-        factory.registerUser("1234", "senha");
         LoginScreen login = new LoginScreen(factory);
     }
 }
